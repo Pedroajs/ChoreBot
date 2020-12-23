eaa57c03b4e9befa5cb329d46c99246565b68f1b
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2020-12-23 00:05:14 -0200

    começando a lógica do game

--- main.js
+++ main.js
@@ -5,19 +5,33 @@ const doorimg3 = document.getElementById('door3');
 const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
 const beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
 const spaceDoorPath =  'https://content.codecademy.com/projects/chore-door/images/space.svg';
+
+let openDoor1,
+    openDoor2,
+    openDoor3;
+
+let numClosedDoors = 3;
+const randomChoreDoorGenerator = ()=>{
+    let choreDoor = Math.floor(Math.random() *numClosedDoors);
+
+    if(choreDoor===0){
+
+    }else if(choreDoor===1){
+
+    }else if(choreDoor===2){
+        
+    }
+}
+
 doorimg1.onclick=()=>{
     doorimg1.src = botDoorPath;
-    console.log('fui clicado porta1')
 }
 
 doorimg2.onclick = () =>{
     doorimg2.src=beachDoorPath;
-    console.log('fui clicado porta2')
 
 }
 
 doorimg3.onclick = () =>{
     doorimg3.src= spaceDoorPath;
-    console.log('fui clicado port3')
-
 }
